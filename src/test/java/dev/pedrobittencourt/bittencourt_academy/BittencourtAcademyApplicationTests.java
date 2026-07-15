f008de980ce4fbe0beccc5cb916c23652d5ffc8d
Pedro Bittencourt <pedrovbittencourt@gmail.com>
2026-07-15 11:48:50 -0300

    test: activate test profile for application tests

--- src/test/java/dev/pedrobittencourt/bittencourt_academy/BittencourtAcademyApplicationTests.java
+++ src/test/java/dev/pedrobittencourt/bittencourt_academy/BittencourtAcademyApplicationTests.java
@@ -2,8 +2,10 @@ package dev.pedrobittencourt.bittencourt_academy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BittencourtAcademyApplicationTests {
 
 	@Test
